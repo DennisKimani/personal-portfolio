054b82d5ae839bc088718e1668857cd9a5edf797
DennisKimani <denkimoney@gmial.com>
2017-08-04 15:53:34 +0300

    crud method for creating a new category.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,4 +11,17 @@ class AdminsController < ApplicationController
     @admin = Admin.new
     render :new
   end
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to admins_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def admin_params
+    params.require(:admin).permit(:title, :description)
+  end
 end
