cda06218dccf59b678832c22dc429414bf636425
DennisKimani <denkimoney@gmial.com>
2017-08-04 11:44:07 +0300

    added a column for title and description.

--- db/migrate/20170804082804_create_admins.rb
+++ db/migrate/20170804082804_create_admins.rb
@@ -1,6 +1,8 @@
 class CreateAdmins < ActiveRecord::Migration[5.1]
   def change
     create_table :admins do |t|
+      t.column :title, :string
+      t.column :description, :string
     end
   end
 end
