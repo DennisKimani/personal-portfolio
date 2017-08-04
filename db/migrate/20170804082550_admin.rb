23a42fde14ef2cba6f89df40d32a06feb9ba4bff
DennisKimani <denkimoney@gmial.com>
2017-08-04 11:29:22 +0300

    created a table for my details and info.

--- db/migrate/20170804082550_admin.rb
+++
@@ -1,4 +0,0 @@
-class Admin < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
---
+++ db/migrate/20170804082804_create_admins.rb
@@ -0,0 +1,6 @@
+class CreateAdmins < ActiveRecord::Migration[5.1]
+  def change
+    create_table :admins do |t|
+    end
+  end
+end
