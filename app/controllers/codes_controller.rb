d2dabf96afa26bf511ba9559a6212e9722ce76da
DennisKimani <denkimoney@gmial.com>
2017-08-07 23:34:32 +0300

    added a create method.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -11,4 +11,17 @@ class CodesController < ApplicationController
     @code = Code.new
     render :new
   end
+  def create
+    @code = Code.new(code_params)
+    if @code.save
+      redirect_to codes_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def code_params
+    params.require(:code).permit(:skill, :link)
+  end
 end
