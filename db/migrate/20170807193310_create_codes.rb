1452f98031bfa2c7aa1a5b2bf8bfb58ff50c2172
DennisKimani <denkimoney@gmial.com>
2017-08-07 22:36:36 +0300

    added columns to the table.~

--- db/migrate/20170807193310_create_codes.rb
+++ db/migrate/20170807193310_create_codes.rb
@@ -1,6 +1,8 @@
 class CreateCodes < ActiveRecord::Migration[5.1]
   def change
     create_table :codes do |t|
+      t.column :skill, :string
+      t.column :link, :string
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804082804) do
+ActiveRecord::Schema.define(version: 20170807193310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20170804082804) do
   create_table "admins", force: :cascade do |t|
     t.string "title"
     t.string "description"
+  end
+
+  create_table "codes", force: :cascade do |t|
+    t.string "skill"
+    t.string "link"
   end
 
 end
