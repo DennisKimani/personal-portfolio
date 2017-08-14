a0cfce4d7f8617af83d16d731a51a1559c1abb21
DennisKimani <denkimoney@gmial.com>
2017-08-14 15:11:01 +0300

    added a method for new link.#

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,16 @@
-class LinksController <ApplicationController
- def index
-   @links = Link.all
-   render :index
- end
-def show
-  @link = Link.find(params[:id])
-  render :show
-end 
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+    render :index
+  end
+
+  def show
+    @link = Link.find(params[:id])
+    render :show
+  end
+
+  def new
+    @link = Link.new
+    render :new
+  end
 end
