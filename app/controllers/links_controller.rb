1fd2f5ba815591aa14e5f3861b083c50e35f187f
DennisKimani <denkimoney@gmial.com>
2017-08-14 15:26:23 +0300

    added a create method.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -13,4 +13,19 @@ class LinksController < ApplicationController
     @link = Link.new
     render :new
   end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def link_params
+    params.require(:link).permit(:name, :description, :github_link, :launch)
+  end
 end
