17d00f28b91ccce255ebc3fba5645b8b5e172b1b
DennisKimani <denkimoney@gmial.com>
2017-08-14 10:53:47 +0300

    added a validate code.

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+ describe Link do
+   it { should validate_presence_of :name }
+   it { should validate_presence_of :description }
+   it { should validate_presence_of :github_link }
+   it { should validate_presence_of :launch }
+ end
