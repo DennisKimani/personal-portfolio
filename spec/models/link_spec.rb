efc2f3b6f30cdb1e72317e8f51febdf3be4b1fb3
DennisKimani <denkimoney@gmial.com>
2017-08-16 11:31:42 +0300

    linked two models together.

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,5 @@ require 'rails_helper'
    it { should validate_presence_of :description }
    it { should validate_presence_of :github_link }
    it { should validate_presence_of :launch }
+   it { should belong_to :code }
  end
