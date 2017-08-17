db75332e5027aaba08338fa6b044de82679782aa
DennisKimani <denkimoney@gmial.com>
2017-08-17 16:51:45 +0300

    deleted them as conflict emerged.

--- db/migrate/20170804082804_create_admins.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdmins < ActiveRecord::Migration[5.1]
-  def change
-    create_table :admins do |t|
-      t.column :title, :string
-      t.column :description, :string
-    end
-  end
-end
--- spec/models/admin_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
- describe Admin do
-   it { should validate_presence_of :title, :description }
- end
